93aa653a348b9d2e11f701437a5174fb106e3e35
milssky <2furius@gmail.com>
2020-09-20 22:10:36 +0300

    Проверил express

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Test')
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+console.log('Started');
+app.listen(port);
